1fa638e7b651e40a8a07b9693cea246539da9e57
axxving <ihernandezramirez800@gmail.com>
2024-03-12 21:07:09 -0600

    feat(): Primera app con VueJS mediante CDN
    Co-Authored-By: axxving <160192346+axxving@users.noreply.github.com>

---
+++ 02-bases-vue-js/app.js
@@ -0,0 +1,12 @@
+// Mostrando en consola VueJS
+console.log( Vue)
+
+//
+const app = Vue.createApp({
+    template:`
+    <h1>Hola Mundo</h1>
+    <p>Desde app.js, esta es mi primera app con Vue.JS</p>
+    ` 
+})
+
+app.mount("#myApp")
