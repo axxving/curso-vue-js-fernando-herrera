b1d307314751bd55b861f861548ab1f508c8a526
axxving <ihernandezramirez800@gmail.com>
2024-03-12 21:20:53 -0600

    feat(): Representación declarativa
    Co-Authored-By: axxving <160192346+axxving@users.noreply.github.com>

--- 02-bases-vue-js/app.js
+++ 02-bases-vue-js/app.js
@@ -1,12 +1,22 @@
 // Mostrando en consola VueJS
 console.log( Vue)
 
-//
+
 const app = Vue.createApp({
-    template:`
-    <h1>Hola Mundo</h1>
-    <p>Desde app.js, esta es mi primera app con Vue.JS</p>
-    ` 
+    // template:`
+    // <h1>Hola Mundo</h1>
+    // <p>Desde app.js, esta es mi primera app con Vue.JS</p>
+    // ` 
 })
+
+// app.mount("#myApp")
+
+// const app = Vue.createApp({
+//     template: `
+    // <h1>Hola Mundo</h1>
+    // <p>{{1 + 1}}</p>
+
+//     `
+// })
 
 app.mount("#myApp")
